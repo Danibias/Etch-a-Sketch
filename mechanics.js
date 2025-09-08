5439207cebb0ca7e22eff2c222311db78223f5b2
Danibias <danielebiasetti1@gmail.com>
2025-09-08 18:33:25 +0100

    bugs fixed

--- mechanics.js
+++ mechanics.js
@@ -1,13 +1,6 @@
 // Canvas design
 const grid = document.querySelector(".grid");
 
-// 16 x 16 default grid
-for (let i = 0; i < 16 * 16; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    grid.appendChild(cell);
-};
-
 // Hover effect 
 grid.addEventListener("mouseover", (e) => {
   if (e.target.classList.contains("cell")) {
@@ -17,14 +10,33 @@ grid.addEventListener("mouseover", (e) => {
 
 // Create a new grid layout
 const layout = document.querySelector(".layout");
+
+// Clamps the size of the grid between 1 and 100
+const clampSize = (num) => {
+  const randomNum  = parseInt(num, 10);
+  if (Number.isNaN(randomNum)) return null;
+  return Math.max(1, Math.min(100, randomNum));
+}
+
+const newGrid = (size) => {
+  grid.innerHTML = ""; // Remove the previous grid
+
+// Creates a new grid and resizes cells within
+  for (let i = 0; i < size * size; i++) {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    cell.style.flex = `0 0 calc(100% / ${size})`;
+    grid.appendChild(cell);
+  }
+}
+
 layout.addEventListener("click", (e) => {
-    const size = prompt("Enter new grid size (e.g. 16 for 16x16), but no more than 100:");
-    if (size !== null && !isNaN(size)) {
-        grid.innerHTML = ""; // Clear the actual grid
-        for (let i = 0; i < size * size; i++) {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            grid.appendChild(cell);
-        }
-    };
+  const input = prompt("Enter new grid size (e.g. 16 for '16x16'), but no more than 100:");
+  if (input === null) return;
+  const size = clampSize(input);
+  if(!size) return alert("Enter a number between 1 and 100.");
+  newGrid(size);
+
 });
+
+newGrid(16);
